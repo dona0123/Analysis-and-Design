c74d68412c6b45daf1dd61831ef11bb0c947845e
dona <ehsk0123@naver.com>
2024-06-08 21:18:00 +0900

    [240608] ch15 Facade 학습

--- src/ch15/A3/Main.java
+++ src/ch15/A3/Main.java
@@ -14,7 +14,24 @@ public class Main {
                 <h1 style="text-align: center">%s</h1>
             </body>
         </html>
-        """.formatted(title, message);
+        """.formatted(title, message); // 순서 맞춰 넣어짐 
+        
+
         System.out.print(html);
+
+
+        // name 변수 여러 줄에 적용하기 
+        String name = "pdn"; 
+
+        String x = 
+        """
+        duksung
+        women's 
+        univ.
+        My name is %s
+        """.formatted(name); 
+
+        System.out.print(x);
+        
     }
 }
